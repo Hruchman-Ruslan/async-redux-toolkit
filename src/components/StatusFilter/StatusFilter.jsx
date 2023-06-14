54e685f5551a5d4cfde78b2e19b346b65e21f775
Ruslan <Giovingo92@gmail.com>
2023-06-14 17:59:19 +0300

    the basic structure of the project

---
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -0,0 +1,38 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'components/Button/Button';
+import { statusFilters } from '../../redux/constants';
+
+import css from './StatusFilter.module.css';
+
+import { getStatusFitler } from 'redux/selectors';
+import { setStatusFilter } from 'redux/filtersSlice';
+
+export const StatusFilter = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(getStatusFitler);
+
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
+  return (
+    <div className={css.wrapper}>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
